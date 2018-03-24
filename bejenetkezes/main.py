53bf9aa714287225b6542b59be88a3881808d53e
detandae <36955850+detandae@users.noreply.github.com>
2018-03-24 18:00:40 +0100

    Add files via upload

---
+++ bejenetkezes/main.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append("/usr/local/lib/python3.5/dist-packages")
+sys.path.append("/usr/lib/python3/dist-packages")
+sys.path.append("/home/pi/.local/lib/python2.7/site-packages")
+sys.path.append("/usr/local/lib/python2.7/dist-packages")
+
+from app import app
+from Login import LoginForm
+from Login import login
+from register import register
+from users import users
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000,threaded=True)
+
+
+
+
+
+
+
+
