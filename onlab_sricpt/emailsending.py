d38887905d77e6bca7379edbe772fbd5c7b3b54c
detandae <36955850+detandae@users.noreply.github.com>
2018-05-05 20:34:17 +0200

    Add files via upload
    Nyomkövető négyzet a kliens oldalon rajzolódik

---
+++ onlab_sricpt/emailsending.py
@@ -0,0 +1,68 @@
+import sys
+sys.path.append("/usr/local/lib/python3.5/dist-packages")
+sys.path.append("/usr/lib/python3/dist-packages")
+sys.path.append("/home/pi/.local/lib/python2.7/site-packages")
+sys.path.append("/usr/local/lib/python2.7/dist-packages")
+# Import smtplib for the actual sending function
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+# For guessing MIME type
+import mimetypes
+import email.mime.application
+# Import the email modules we'll need
+import email
+import email.mime
+import timeit
+import time
+
+class EmailSend(object):
+    def __init__(self):
+        self.msg = email.mime.multipart.MIMEMultipart()
+        self.msg['Subject'] = '[Behatolás]'
+        self.msg['From'] = 'DjVajda@gmail.com'
+        self.msg['To'] = "DjVajda@gmail.com"
+        self.gmail_user = 'DjVajda69@gmail.com'
+        self.gmail_password = 'Stupor69'
+        self.sent_from = self.gmail_user
+        self.body = email.mime.text.MIMEText("Behatolás történt!")
+        self.msg.attach(self.body)
+        self.server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        self.time=0
+    def Send(self,to):
+        currenttime=time.time()
+        if (currenttime-self.time)>5:
+            self.time=currenttime
+            try:
+                self.server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+                self.server.ehlo()        
+                self.server.login(self.gmail_user, self.gmail_password)
+              
+                self.server.sendmail(self.sent_from,to, self.msg.as_string())
+                self.server.close()
+                print('Email sent!')
+            except:
+                print('Something went wrong...')
+    def SetAdress(self,address):
+         self.msg['To'] = adress
+'''
+EmailClass=MyMail()
+to =['detariandras@gmail.com','DjVajda@gmail.com']
+while(1):
+    EmailClass.Send(to)
+    time.sleep(1)
+'''    
+    
+    
+    
+    
+    
+                
+        
+        
+        
+        
+        
+        
+        
+        
---
+++ onlab_sricpt/pycamera_stream.py
@@ -0,0 +1,62 @@
+import sys
+sys.path.append("/usr/local/lib/python3.5/dist-packages")
+sys.path.append("/usr/lib/python3/dist-packages")
+sys.path.append("/home/pi/Desktop/python/webapp6")
+
+from importlib import import_module
+import os
+from flask import Flask, render_template, Response
+from werkzeug.urls import url_parse
+from flask import Flask,url_for, jsonify
+from tables import User
+from flask_wtf import FlaskForm
+from app import db
+from app import app
+from flask_login import LoginManager, login_user, current_user, login_required, logout_user,UserMixin
+from flask import flash, redirect
+from flask import render_template
+# import camera driver
+"""
+if os.environ.get('CAMERA'):
+    Camera = import_module('camera_' + os.environ['CAMERA']).Camera
+else:
+    from camera import Camera
+"""
+# Raspberry Pi camera module (requires picamera package)
+from camera_pi import Camera
+from app import MovDetection
+
+MyCamera=Camera()
+
+@app.route('/SecurityCamera')
+@login_required
+
+
+def CameraPage():
+    """Video streaming home page."""
+    
+    return render_template('camera.html')
+
+@app.route('/get_word')
+def get_word():
+    '''Return data in json format'''
+    rect=MovDetection.RectParams()   
+    return jsonify(rect)
+
+
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+@login_required
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(MyCamera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
